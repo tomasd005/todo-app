ae4912ab47902973b22cc328fd905cfd38b7139e
Tomás Dias <a107323@alunos.uminho.pt>
2026-07-02 12:09:42 +0000

    Add todo-domain/src/main/java/com/todolist/domain/model/User.java

---
+++ todo-domain/src/main/java/com/todolist/domain/model/User.java
@@ -0,0 +1,57 @@
+package com.todolist.domain.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+    private final String id;
+    private String username;
+    private String passwordHash;
+
+    public User(String username, String passwordHash) {
+        this.id = UUID.randomUUID().toString();
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
