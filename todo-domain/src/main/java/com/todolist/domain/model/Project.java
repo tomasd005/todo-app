352453ab44e1d91d3709f9b8ad7dd51a7da12b4c
Tomás Dias <a107323@alunos.uminho.pt>
2026-07-02 12:09:37 +0000

    Add todo-domain/src/main/java/com/todolist/domain/model/Project.java

---
+++ todo-domain/src/main/java/com/todolist/domain/model/Project.java
@@ -0,0 +1,63 @@
+package com.todolist.domain.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Project {
+    private final String id;
+    private String name;
+    private String description;
+
+    public Project(String name, String description) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Project)) return false;
+        Project project = (Project) o;
+        return Objects.equals(id, project.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Project clone() {
+        Project clone = new Project(name, description);
+        return clone;
+    }
+}
